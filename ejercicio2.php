640f94d590100ae53ad506f48940450b37780018
antonio-sanchezz <antonio.sanchez.espinosa.al@iespoligonosur.org>
2022-02-04 10:26:32 +0100

    ejercicio 2

--- ejercicio2.php
+++ ejercicio2.php
@@ -2,25 +2,22 @@
 // Vaciamos algunas variables
 $error = "";
 $resultado = "";
-$num1 = "";
-$num2 = "";
+$num = "";
 
 // Iniciamos el cliente SOAP
 // Escribimos la dirección donde se encuentra el servicio
-$url = "http://192.168.129.80/DWES/DWES-UD7/ejercicio1.php";
-$uri = "http://192.168.129.80/DWES/DWES-UD7/";
+$url = "http://192.168.129.125/DWS/DWES-UD7/ejercicio2.php";
+$uri = "http://192.168.129.125/DWS/DWES-UD7/";
 $cliente = new SoapClient(null, array('location' => $url, 'uri' => $uri));
 
 // Ejecutamos las siguientes líneas al enviar el formulario.
 if (isset($_POST['enviar'])) {
     // Establecemos los parámetros de envío
-    if (is_int((int)$_POST['num1']) && is_int((int)$_POST['num2'])) {
-        $num1 = (int)$_POST['num1'];
-        $num2 = (int)$_POST['num2'];
-        // Si los parámetros son correctos, llamamos a la función suma de ejercicio1.php
-        $resultado = "La suma es: " . $cliente->suma($num1, $num2);
+    if (is_int((int)$_POST['num'])) {
+        $num = (int)$_POST['num'];
+        $resultado = "El número es: " . $cliente->devuelveParidad($num);
     } else {
-        $error = "<strong>Error:</strong> Debes introducir dos números enteros.";
+        $error = "<strong>Error:</strong> Debes introducir números enteros.";
     }
 }
 ?>
@@ -28,16 +25,15 @@ if (isset($_POST['enviar'])) {
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Calcular Suma - Servicio Web + PHP + SOAP</title>
+        <title>Calcular Si es par o impar - Servicio Web + PHP + SOAP</title>
     </head>
 <body>
-    <h1>Obtener Suma</h1>
+    <h1>Obtener Si es par o impar</h1>
     <h2>Servicio Web + PHP + SOAP</h2>
     <form action="ejercicio2.php" method="post">
     <?php
-        print "<input type='number' name='num1' value='$num1'>";
-        print "<input type='number' name='num2' value='$num2'>";
-        print "<input type='submit' name='enviar' value='Sumar'>";
+        print "<input type='number' name='num' value='$num'>";
+        print "<input type='submit' name='enviar' value='Comprobar'>";
         print "<p class='error'>$error</p>";
         print "<p style='font-size: 12pt;font-weight: bold;color: #0066CC;'>$resultado</p>";
     ?>
